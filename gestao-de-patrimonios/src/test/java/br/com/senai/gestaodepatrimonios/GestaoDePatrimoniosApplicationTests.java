98457a7c995b2aa526457b332056ac8e255c2027
epicestudar <viniciusftza@gmail.com>
2024-05-16 16:02:10 -0300

    sada

---
+++ gestao-de-patrimonios/src/test/java/br/com/senai/gestaodepatrimonios/GestaoDePatrimoniosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.gestaodepatrimonios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDePatrimoniosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
